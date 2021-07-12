eb52b0a0cf93060674c68f1dd09d0b3b9298a0d4
Guilherme Viana <81631546+gui-brv@users.noreply.github.com>
2021-07-12 19:56:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,6 @@
       $('a.menu').removeClass("active");
       $(this).addClass("active");
   });
-
-    $(document).on('click', '.nav-menu a, .scrollto', function(e) {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        e.preventDefault();
-        var target = $(this.hash);
-        if (target.length) {
-  
-          var scrollto = target.offset().top;
-  
-          $('html, body').animate({
-            scrollTop: scrollto
-          }, 1500, 'easeInOutExpo');
-  
-          if ($(this).parents('.nav-menu, .mobile-nav').length) {
-            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-            $(this).closest('li').addClass('active');
-          }
-  
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          }
-          return false;
-        }
-      }
-    });
   
     $(document).ready(function() {
       if (window.location.hash) {
